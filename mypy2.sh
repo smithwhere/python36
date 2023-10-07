e16e684e04b68f8c65e68afad5f2944e89559bdf
smithwhere <96646318+smithwhere@users.noreply.github.com>
2023-10-07 21:58:55 +0800

    Create mypy2.sh

---
+++ mypy2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt-get update -y
+apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tar.xz && tar zxvf Python-3.12.0.tar.xz && cd Python-3.12.0
+./configure
+make&& make install
+cd ..
+rm -rf Python-3.12.0 Python-3.12.0.tar.xz
