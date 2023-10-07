6070901235db438e3fc71c59a8649ce2bc7492e2
smithwhere <96646318+smithwhere@users.noreply.github.com>
2023-10-07 23:16:29 +0800

    Update mypy0.sh

--- mypy0.sh
+++ mypy0.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 apt-get update -y
 apt-get install -y python-is-python3 python3-pip supervisor build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-wget https://github.com/smithwhere/python36/blob/main/Python-3.6.4.tar.gz && tar zxvf Python-3.6.4.tar.gz && cd Python-3.6.4
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tar.xz && tar zxvf Python-3.12.0.tar.xz && cd Python-3.12.0
 ./configure
 make&& make install
 cd ..
-rm -rf Python-3.6.4 Python-3.6.4.tar.gz
+rm -rf Python-3.12.0 Python-3.12.0.tar.xz
