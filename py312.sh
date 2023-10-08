f56a8b38c96142e588a242f3c26ef73e081d6a36
smithwhere <96646318+smithwhere@users.noreply.github.com>
2023-10-08 20:33:31 +0800

    Create py312.sh

---
+++ py312.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get update -y
+apt-get install -y python-is-python3 python3-pip supervisor build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz && tar zxvf Python-3.12.0.tgz && cd Python-3.12.0
+./configure
+make&& make install
+cd ..
+rm -rf Python-3.12.0 Python-3.12.0.tgz
+rm -rf  /usr/bin/python
+ln -s /usr/bin/python3.12 /usr/bin/python
