7d426b347282b82c5d3fdec32d558ed7b30d5e8d
smithwhere <96646318+smithwhere@users.noreply.github.com>
2023-10-08 20:20:20 +0800

    Update testpy.sh

--- testpy.sh
+++ testpy.sh
@@ -2,7 +2,7 @@
 apt-get update -y
 apt-get install -y python-is-python3 python3-pip supervisor build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 wget wget https://www.python.org/ftp/python/3.9.7/Python-3.9.7.tgz && tar zxvf Python-3.9.7.tgz && cd Python-3.9.7
-./configure --enable-optimizations
+./configure
 make&& make install
 cd ..
 rm -rf Python-3.9.7 Python-3.9.7.tar.gz
