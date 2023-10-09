4a3f3ac2c584fa5feb2b028059bcc0cbda711dc8
smithwhere <96646318+smithwhere@users.noreply.github.com>
2023-10-09 22:31:47 +0800

    Create 111.sh

---
+++ 111.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt-get update && apt upgrade -y
+apt-get install libreadline-dev libc6-dev libffi-dev build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+wget wget https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz && tar zxvf Python-2.7.13.tgz && cd Python-2.7.13
+./configure
+make&& make install
+cd ..
+rm -rf Python-2.7.13 Python-2.7.13.tgz
