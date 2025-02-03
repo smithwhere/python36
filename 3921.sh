994880522482da93137eb609b5a621a2830a8763
smithwhere <96646318+smithwhere@users.noreply.github.com>
2025-02-03 15:11:56 +0800

    Add files via upload

---
+++ 3921.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt-get update && apt upgrade -y
+apt-get install -y python-is-python3 python3-pip supervisor libreadline-dev libc6-dev libffi-dev build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz && tar zxvf Python-3.7.3.tgz && cd Python-3.7.3
+./configure
+make&& make install
+cd ..
+rm -rf Python-3.7.3 Python-3.7.3.tgz
