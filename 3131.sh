69d946d10135f7ac2ff44a3f6844ee7d5eca2c03
smithwhere <96646318+smithwhere@users.noreply.github.com>
2025-02-03 17:00:18 +0800

    Delete 3131.sh

--- 3131.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-apt-get update && apt upgrade -y
-apt-get install -y python-is-python3 python3-pip supervisor libreadline-dev libc6-dev libffi-dev build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-wget https://www.python.org/ftp/python/3.13.1/Python-3.13.1.tgz && tar zxvf Python-3.13.1.tgz && cd Python-3.13.1
-./configure
-make&& make install
-ln -sf /usr/local/bin/python3.13 /usr/bin/python
-cd ..
-rm -rf Python-3.13.1 Python-3.13.1.tgz
