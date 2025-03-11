3a125cccc70182fc8afaae42530a951bd16c63aa
smithwhere <96646318+smithwhere@users.noreply.github.com>
2025-03-11 20:21:04 +0800

    Update u2718.sh

--- u2718.sh
+++ u2718.sh
@@ -2,7 +2,7 @@
 apt-get update && apt upgrade -y
 sudo apt-get install build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz && tar zxvf Python-2.7.18.tgz && cd Python-2.7.18
-sudo apt-get install build-essential zlib1g-dev libbz2-dev libssl-dev libncurses5-dev  libsqlite3-dev libreadline-dev tk-dev libgdbm-dev libdb-dev libpcap-dev xz-utils libexpat1-dev   liblzma-dev libffi-dev  libc6-dev
+sudo apt-get install build-essential zlib1g-dev libbz2-dev libssl-dev libncurses5-dev  libsqlite3-dev libreadline-dev tk-dev libgdbm-dev libdb-dev libpcap-dev xz-utils libexpat1-dev liblzma-dev libffi-dev libc6-dev
 ./configure
 make && make install
 cd ..
