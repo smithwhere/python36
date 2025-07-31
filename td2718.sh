83569ad16307f7beebe58b4177ff5fa5b08aef07
smithwhere <96646318+smithwhere@users.noreply.github.com>
2025-07-31 13:53:24 +0800

    Create td2718.sh

---
+++ td2718.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+apt-get update && apt upgrade -y
+wget https://github.com/smithwhere/python36/releases/download/Python-2.7.18/Python-2.7.18.tgz && tar zxvf Python-2.7.18.tgz && cd Python-2.7.18
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libreadline-dev libdb-dev libpcap-dev xz-utils libexpat1-dev liblzma-dev libffi-dev checkinstall libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+./configure
+make && make install
+cd ..
