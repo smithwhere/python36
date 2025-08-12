3dd366ecdfe4140171fa5a7c61f870b5449c4300
smithwhere <96646318+smithwhere@users.noreply.github.com>
2025-08-12 21:53:59 +0800

    Update 2718.sh

--- 2718.sh
+++ 2718.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get update -y &&apt upgrade gcc -y && apt upgrade -y
+apt-get update -y && apt upgrade gcc -y && apt upgrade -y
 wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz && tar zxvf Python-2.7.18.tgz && cd Python-2.7.18
 sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libreadline-dev libdb-dev libpcap-dev xz-utils libexpat1-dev liblzma-dev libffi-dev checkinstall libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
 ./configure --enable-optimizations
