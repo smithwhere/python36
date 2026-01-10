f8a2ea20fe252ccc71db0eb3f4a0788e003b084c
smithwhere <96646318+smithwhere@users.noreply.github.com>
2026-01-10 10:48:01 +0800

    Update td2718.sh

--- td2718.sh
+++ td2718.sh
@@ -4,7 +4,6 @@ sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev l
 wget https://github.com/smithwhere/python36/releases/download/Python-2.7.18/Python-2.7.18.tgz && tar zxvf Python-2.7.18.tgz && cd Python-2.7.18
 ./configure --prefix=/opt/python --enable-unicode=ucs4
 make -j$(nproc) && sudo make install
-sudo ln -s /opt/python/bin/python2.7 /usr/local/bin/python
 cd ..
 rm -rf Python-2.7.18.tgz
 python --version
