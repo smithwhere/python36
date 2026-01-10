7994afd47defd2f221c2839c63346af194d863a5
smithwhere <96646318+smithwhere@users.noreply.github.com>
2026-01-10 12:41:56 +0800

    Update td2718.sh

--- td2718.sh
+++ td2718.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get update && apt upgrade
+apt-get update && apt-get upgrade -y
 apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
 wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz && tar -xzvf Python-2.7.18.tgz && cd Python-2.7.18
 ./configure
