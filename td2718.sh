60fd04a329aa373bb1cc2383e6c5cfa40181b6c1
smithwhere <96646318+smithwhere@users.noreply.github.com>
2026-01-10 10:34:32 +0800

    Update td2718.sh

--- td2718.sh
+++ td2718.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update && apt upgrade
-sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 wget https://github.com/smithwhere/python36/releases/download/Python-2.7.18/Python-2.7.18.tgz && tar zxvf Python-2.7.18.tgz && cd Python-2.7.18
 ./configure --prefix=/opt/python --enable-unicode=ucs4
 make -j$(nproc) && sudo make install
